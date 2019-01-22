c380df36858a05449f5de0391d20452ed22ae9a8
julian grinn <juliantshivet@gmail.com>
2019-01-22 16:10:35 +0100

    controller_admin

---
+++ application/models/Admin_model.php
@@ -0,0 +1,31 @@
+<?php
+class Admin_model extends CI_Model{
+
+    private $TableAdmin = 'admin';
+    function __construct(){
+
+        parent :: __construct();
+    }
+    public function get_data($nom,$pwd,$level){
+        
+        $this->db->set('username',$nom);
+        $this->db->set('pwd',$pwd);
+        $this->db->set('level',$level);
+        return $this->db->insert($this->TableAdmin);
+
+    }
+    public function checking($username,$pwd){
+        return $this->db->where(array('username'=>$username,'pwd'=>$pwd))->get($this->TableAdmin)->result();
+
+    }
+    public function voir_admin(){
+        return $this->db->select()
+        ->get($this->TableAdmin)
+        ->result();
+
+
+    }
+
+}
+
+?>
